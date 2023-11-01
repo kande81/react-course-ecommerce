cca2e785640f0419b92adb145cab8168ad2b028f
gilles <yannickbabou@gmail.com>
2023-10-31 21:54:27 -0700

    completed the checkout page along the checkout cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,29 +1,41 @@
-import { useContext } from "react";
+import { useContext } from "react"; // Importing the useContext hook from React
+import { useNavigate } from "react-router-dom"; // Importing the useNavigate hook from React Router
 
-import { CartContext } from "../../contexts/cart.context";
+import { CartContext } from "../../contexts/cart.context"; // Importing the CartContext from the cart.context.js file
 
-import Button from "../button/button.component";
-import CartItem from "../cart-item/cart-item.component";
+import Button from "../button/button.component"; // Importing the Button component
+import CartItem from "../cart-item/cart-item.component"; // Importing the CartItem component
 
-import "./cart-dropdown.styles.scss";
+import "./cart-dropdown.styles.scss"; // Importing the styles for the CartDropdown component
 
 const CartDropdown = () => {
-  const { cartItems } = useContext(CartContext);
+  const { cartItems } = useContext(CartContext); // Using the useContext hook to get the cartItems from the CartContext
+  const navigate = useNavigate(); // Using the useNavigate hook to get the navigate function, which is used to navigate to a different page
+
+  const goToCheckoutHandler = () => {
+    // Function to handle the click event on the "GO TO CHECKOUT" button
+    navigate("/checkout"); // Navigating to the checkout page
+  };
 
   return (
     <div className="cart-dropdown-container">
+      {" "}
       <div className="cart-items">
-        {cartItems.length ? (
-          cartItems.map((cartItem) => (
-            <CartItem key={cartItem.id} cartItem={cartItem} />
-          ))
+        {" "}
+        {cartItems.length ? ( // If there are cart items
+          cartItems.map(
+            (
+              cartItem // Map through the cart items and render the CartItem component for each item
+            ) => <CartItem key={cartItem.id} cartItem={cartItem} />
+          )
         ) : (
-          <span className="empty-message">Your cart is empty</span>
+          // If there are no cart items
+          <span className="empty-message">Your cart is empty</span> // Display a message saying the cart is empty
         )}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
 
-export default CartDropdown;
+export default CartDropdown; // Exporting the CartDropdown component
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,14 +1,11 @@
 import { createContext, useState, useEffect } from "react";
 
-// This function takes in an array of cart items and a product to add to the cart
-export const addCartItem = (cartItems, productToAdd) => {
-  // Check if the product to add already exists in the cart
+const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === productToAdd.id
   );
 
   if (existingCartItem) {
-    // If the product already exists in the cart, increment its quantity by 1
     return cartItems.map((cartItem) =>
       cartItem.id === productToAdd.id
         ? { ...cartItem, quantity: cartItem.quantity + 1 }
@@ -16,22 +13,58 @@ export const addCartItem = (cartItems, productToAdd) => {
     );
   }
 
-  // If the product does not exist in the cart, add it with a quantity of 1
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
+
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  // find the cart item to remove
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  // check if quantity is equal to 1, if it is remove that item from the cart
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  // return back cartitems with matching cart item with reduced quantity
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
+// This function, clearCartItem, takes in two arguments:
+// 1. cartItems: an array of items currently in the cart
+// 2. cartItemToClear: the item that needs to be removed from the cart
+// It returns a new array that does not include the cartItemToClear.
+// This is done by filtering out the item that has the same id as cartItemToClear.
+const clearCartItem = (cartItems, cartItemToClear) =>
+  cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
 export const CartContext = createContext({
   isCartOpen: false,
-  setIsOpen: () => {},
+  setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
   cartCount: 0,
+  cartTotal: 0,
 });
 
+// when using useEffect it is best practice to have each useEffect do one thing. that is why we have two useEffects here
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
+  // This useEffect hook is responsible for calculating the total quantity of items in the cart.
+  // It does this by reducing over the cartItems array and summing up the quantity of each item.
+  // The total quantity is then set to the state variable cartCount.
+  // This useEffect hook will run every time the cartItems array changes.
   useEffect(() => {
     const newCartCount = cartItems.reduce(
       (total, cartItem) => total + cartItem.quantity,
@@ -40,15 +73,38 @@ export const CartProvider = ({ children }) => {
     setCartCount(newCartCount);
   }, [cartItems]);
 
-  const addItemToCart = (product) =>
-    setCartItems(addCartItem(cartItems, product));
+  // This useEffect hook is responsible for calculating the total price of items in the cart.
+  // It does this by reducing over the cartItems array and summing up the product of the quantity and price of each item.
+  // The total price is then set to the state variable cartTotal.
+  // This useEffect hook will run every time the cartItems array changes.
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
+      0
+    );
+    setCartTotal(newCartTotal);
+  }, [cartItems]);
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const removeItemFromCart = (cartItemToRemove) => {
+    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  };
+
+  const clearItemFromCart = (cartItemToClear) => {
+    setCartItems(clearCartItem(cartItems, cartItemToClear));
+  };
 
   const value = {
     isCartOpen,
     setIsCartOpen,
-    cartItems,
     addItemToCart,
+    removeItemFromCart,
+    clearItemFromCart,
+    cartItems,
     cartCount,
+    cartTotal,
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
