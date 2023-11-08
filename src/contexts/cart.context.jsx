93256ee1f3eb64a1d522fd4cdac5c955eb76d9bd
gilles <yannickbabou@gmail.com>
2023-11-07 21:06:06 -0800

    set up the cart context to use reducers

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,6 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useReducer } from "react";
+
+import { createAction } from "../utils/reducer/reducer.utils";
 
 const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
@@ -35,11 +37,34 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
   );
 };
 
-// This function, clearCartItem, takes in two arguments:
-// 1. cartItems: an array of items currently in the cart
-// 2. cartItemToClear: the item that needs to be removed from the cart
-// It returns a new array that does not include the cartItemToClear.
-// This is done by filtering out the item that has the same id as cartItemToClear.
+const CART_ACTION_TYPES = {
+  SET_IS_CART_OPEN: "SET_IS_CART_OPEN",
+  SET_CART_ITEMS: "SET_CART_ITEMS",
+  SET_CART_COUNT: "SET_CART_COUNT",
+  SET_CART_TOTAL: "SET_CART_TOTAL",
+};
+
+const INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+  cartTotal: 0,
+};
+
+const cartReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CART_ACTION_TYPES.SET_CART_ITEMS:
+      return {
+        ...state,
+        ...payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in cartReducer`);
+  }
+};
+
 const clearCartItem = (cartItems, cartItemToClear) =>
   cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
 
@@ -54,53 +79,54 @@ export const CartContext = createContext({
   cartTotal: 0,
 });
 
-// when using useEffect it is best practice to have each useEffect do one thing. that is why we have two useEffects here
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
-  const [cartCount, setCartCount] = useState(0);
-  const [cartTotal, setCartTotal] = useState(0);
 
-  // This useEffect hook is responsible for calculating the total quantity of items in the cart.
-  // It does this by reducing over the cartItems array and summing up the quantity of each item.
-  // The total quantity is then set to the state variable cartCount.
-  // This useEffect hook will run every time the cartItems array changes.
-  useEffect(() => {
+  const [{ cartCount, cartTotal, cartItems }, dispatch] = useReducer(
+    cartReducer,
+    INITIAL_STATE
+  );
+
+  const updateCartItemsReducer = (cartItems) => {
     const newCartCount = cartItems.reduce(
       (total, cartItem) => total + cartItem.quantity,
       0
     );
-    setCartCount(newCartCount);
-  }, [cartItems]);
 
-  // This useEffect hook is responsible for calculating the total price of items in the cart.
-  // It does this by reducing over the cartItems array and summing up the product of the quantity and price of each item.
-  // The total price is then set to the state variable cartTotal.
-  // This useEffect hook will run every time the cartItems array changes.
-  useEffect(() => {
     const newCartTotal = cartItems.reduce(
       (total, cartItem) => total + cartItem.quantity * cartItem.price,
       0
     );
-    setCartTotal(newCartTotal);
-  }, [cartItems]);
-  const addItemToCart = (productToAdd) => {
-    setCartItems(addCartItem(cartItems, productToAdd));
+
+    const payload = {
+      cartItems,
+      cartCount: newCartCount,
+      cartTotal: newCartTotal,
+    };
+
+    dispatch(createAction(CART_ACTION_TYPES.SET_CART_ITEMS, payload));
   };
 
-  const removeItemFromCart = (cartItemToRemove) => {
-    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  const addItemToCart = (productToAdd) => {
+    const newCartItems = addCartItem(cartItems, productToAdd);
+    updateCartItemsReducer(newCartItems);
+  };
+
+  const removeItemToCart = (cartItemToRemove) => {
+    const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+    updateCartItemsReducer(newCartItems);
   };
 
   const clearItemFromCart = (cartItemToClear) => {
-    setCartItems(clearCartItem(cartItems, cartItemToClear));
+    const newCartItems = clearCartItem(cartItems, cartItemToClear);
+    updateCartItemsReducer(newCartItems);
   };
 
   const value = {
     isCartOpen,
     setIsCartOpen,
     addItemToCart,
-    removeItemFromCart,
+    removeItemToCart,
     clearItemFromCart,
     cartItems,
     cartCount,
@@ -109,3 +135,118 @@ export const CartProvider = ({ children }) => {
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
+
+/* This is the code without using reducers */
+
+// import { createContext, useState, useEffect } from "react";
+
+// const addCartItem = (cartItems, productToAdd) => {
+//   const existingCartItem = cartItems.find(
+//     (cartItem) => cartItem.id === productToAdd.id
+//   );
+
+//   if (existingCartItem) {
+//     return cartItems.map((cartItem) =>
+//       cartItem.id === productToAdd.id
+//         ? { ...cartItem, quantity: cartItem.quantity + 1 }
+//         : cartItem
+//     );
+//   }
+
+//   return [...cartItems, { ...productToAdd, quantity: 1 }];
+// };
+
+// const removeCartItem = (cartItems, cartItemToRemove) => {
+//   // find the cart item to remove
+//   const existingCartItem = cartItems.find(
+//     (cartItem) => cartItem.id === cartItemToRemove.id
+//   );
+
+//   // check if quantity is equal to 1, if it is remove that item from the cart
+//   if (existingCartItem.quantity === 1) {
+//     return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+//   }
+
+//   // return back cartitems with matching cart item with reduced quantity
+//   return cartItems.map((cartItem) =>
+//     cartItem.id === cartItemToRemove.id
+//       ? { ...cartItem, quantity: cartItem.quantity - 1 }
+//       : cartItem
+//   );
+// };
+
+// This function, clearCartItem, takes in two arguments:
+// 1. cartItems: an array of items currently in the cart
+// 2. cartItemToClear: the item that needs to be removed from the cart
+// It returns a new array that does not include the cartItemToClear.
+// This is done by filtering out the item that has the same id as cartItemToClear.
+// const clearCartItem = (cartItems, cartItemToClear) =>
+//   cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
+// export const CartContext = createContext({
+//   isCartOpen: false,
+//   setIsCartOpen: () => {},
+//   cartItems: [],
+//   addItemToCart: () => {},
+//   removeItemFromCart: () => {},
+//   clearItemFromCart: () => {},
+//   cartCount: 0,
+//   cartTotal: 0,
+// });
+
+// // when using useEffect it is best practice to have each useEffect do one thing. that is why we have two useEffects here
+// export const CartProvider = ({ children }) => {
+//   const [isCartOpen, setIsCartOpen] = useState(false);
+//   const [cartItems, setCartItems] = useState([]);
+//   const [cartCount, setCartCount] = useState(0);
+//   const [cartTotal, setCartTotal] = useState(0);
+
+// This useEffect hook is responsible for calculating the total quantity of items in the cart.
+// It does this by reducing over the cartItems array and summing up the quantity of each item.
+// The total quantity is then set to the state variable cartCount.
+// This useEffect hook will run every time the cartItems array changes.
+// useEffect(() => {
+//   const newCartCount = cartItems.reduce(
+//     (total, cartItem) => total + cartItem.quantity,
+//     0
+//   );
+//   setCartCount(newCartCount);
+// }, [cartItems]);
+
+// This useEffect hook is responsible for calculating the total price of items in the cart.
+// It does this by reducing over the cartItems array and summing up the product of the quantity and price of each item.
+// The total price is then set to the state variable cartTotal.
+// This useEffect hook will run every time the cartItems array changes.
+// useEffect(() => {
+//   const newCartTotal = cartItems.reduce(
+//     (total, cartItem) => total + cartItem.quantity * cartItem.price,
+//     0
+//   );
+//   setCartTotal(newCartTotal);
+// }, [cartItems]);
+
+// const addItemToCart = (productToAdd) => {
+//   setCartItems(addCartItem(cartItems, productToAdd));
+// };
+
+//   const removeItemFromCart = (cartItemToRemove) => {
+//     setCartItems(removeCartItem(cartItems, cartItemToRemove));
+//   };
+
+//   const clearItemFromCart = (cartItemToClear) => {
+//     setCartItems(clearCartItem(cartItems, cartItemToClear));
+//   };
+
+//   const value = {
+//     isCartOpen,
+//     setIsCartOpen,
+//     addItemToCart,
+//     removeItemFromCart,
+//     clearItemFromCart,
+//     cartItems,
+//     cartCount,
+//     cartTotal,
+//   };
+
+//   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+// };
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,25 +1,40 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 import {
   onAuthStateChangedListener,
-  signOutUser,
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase.utils";
 
-// see this as the actual value that you want to access
-// we will import this in the components that need it
 export const UserContext = createContext({
-  currentUser: null,
   setCurrentUser: () => null,
+  currentUser: null,
 });
 
-// see this as the provider that will provide the value to the components
-// in the index.js file, we wrap the entire app in this provider
-export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
 
-  // when we pass in an empty bracket as the second argument to the useEffect hook, it means that the useEffect hook will only run once when the component mounts.
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+export const UserProvider = ({ children }) => {
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (user) =>
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
@@ -28,8 +43,51 @@ export const UserProvider = ({ children }) => {
       setCurrentUser(user);
     });
 
-    return unsubscribe;
+    return unsubscribe; // this is the cleanup function that will run when the component unmounts. unsubscribe is actually a function. In a useEffect hook, you can return a function that will run when the component unmounts. This is called the cleanup function.
   }, []);
-  // the value prop here which is currentUser and setCurrentUser as an object is what will be returned when we import the UserContext in the components that need it by calling useContext(UserContext)
+
+  const value = {
+    currentUser,
+    setCurrentUser,
+  };
+
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
+// this is the code without using reducers
+
+// import { createContext, useState, useEffect } from "react";
+
+// import {
+//   onAuthStateChangedListener,
+//   signOutUser,
+//   createUserDocumentFromAuth,
+// } from "../utils/firebase/firebase.utils";
+
+// // see this as the actual value that you want to access
+// // we will import this in the components that need it
+// export const UserContext = createContext({
+//   currentUser: null,
+//   setCurrentUser: () => null,
+// });
+
+// // see this as the provider that will provide the value to the components
+// // in the index.js file, we wrap the entire app in this provider
+// export const UserProvider = ({ children }) => {
+//   const [currentUser, setCurrentUser] = useState(null);
+//   const value = { currentUser, setCurrentUser };
+
+//   // when we pass in an empty bracket as the second argument to the useEffect hook, it means that the useEffect hook will only run once when the component mounts.
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChangedListener((user) => {
+//       if (user) {
+//         createUserDocumentFromAuth(user);
+//       }
+//       setCurrentUser(user);
+//     });
+
+//     return unsubscribe;
+//   }, []);
+//   // the value prop here which is currentUser and setCurrentUser as an object is what will be returned when we import the UserContext in the components that need it by calling useContext(UserContext)
+//   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+// };
